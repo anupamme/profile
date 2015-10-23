445885d481c4525bd7662c6e127cd836e7cd1f5f
finalkick <1rn10mca31@gmail.com>
2015-10-23 16:49:19 +0530

    updated code
    Angular binding

--- js/app.js
+++ js/app.js
@@ -6,35 +6,53 @@ avenue.config(function ($routeProvider) {
         // route for the home page
         .when('/', {
             templateUrl: 'templates/home.html',
-            controller:'homeController'
+            controller: 'homeController'
         }).
-         when('/detailedView', {
-             templateUrl: 'templates/detailedView.html',
-             controller: 'detailedViewController'
-         }).
         when('/about', {
-              templateUrl: 'templates/about.html',
-              controller: 'aboutController'
-          }).
-        when ('/alchemy', {
-            templateUrl: 'templates/detail-alchemy.html',
+            templateUrl: 'templates/about.html',
+            controller: 'aboutController'
+        }).
+        when('/detailedView/:id', {
+            templateUrl: 'templates/detailView.html',
             controller: 'detailedViewController'
         }).
+        when('/Error', {
+        templateUrl: 'templates/error.html'
+    }).
         otherwise({
-        redirectTo: '/'
+            redirectTo: '/'
         });
 });
 
-avenue.controller("homeController", function () {
-
-});
-
-avenue.controller("detailedViewController", function () {
-    when ('/alchemy', {
-        templateUrl: 'templates/detail-alchemy.html',
-        controller: 'detailedViewController'
-    }).
-    otherwise({
-        redirectTo: '/'
+avenue.controller("homeController", ['$scope', '$http', function ($scope, $http) {
+    $http.get("templates/homepage.json", { cache: true }).success(function (data) {
+        $scope.items = data.items;
+    }).error(function (data) {
+        $window.location.href = '#/Error';
     });
-});
+}]);
+
+avenue.controller("detailedViewController", ['$scope', '$http', '$window', '$routeParams', function ($scope, $http, $window, $routeParams) {
+    $http.get("templates/detailedView.json", { cache: true }).success(function (data) {
+        for (var i = 0; i < data.items.length; i++) {
+            if (data.items[i].detailedLinkID == $routeParams.id) {
+                $scope.item = data.items[i];
+                break;
+            }
+        }
+    }).error(function (data) {
+        $window.location.href = '#/Error';
+    });
+    $http.get("templates/homepage.json", { cache: true }).success(function (data) {
+        $scope.list = data.items;
+    }).error(function (data) {
+        $scope.noDataFound = "No data found";
+       // $window.location.href = '#/Error';
+    });
+}]);
+
+avenue.filter("nl2br", ['$sce', function ($sce) {
+    return function (text) {
+        return $sce.trustAsHtml(text);
+    };
+}]);
