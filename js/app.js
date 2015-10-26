613e7e573aeb42c604ce20eca92423a19edb3f5b
finalkick <1rn10mca31@gmail.com>
2015-10-26 14:00:17 +0530

    updated video
    updated video

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+ (function (angular) {
 var avenue = angular.module('avenue', ['ngRoute']);
 
 avenue.config(function ($routeProvider) {
@@ -33,17 +34,30 @@ avenue.controller("homeController", ['$scope', '$http', function ($scope, $http)
 }]);
 
 avenue.controller("detailedViewController", ['$scope', '$http', '$window', '$routeParams', function ($scope, $http, $window, $routeParams) {
-    $http.get("templates/detailedView.json", { cache: true }).success(function (data) {
-        for (var i = 0; i < data.items.length; i++) {
-            if (data.items[i].detailedLinkID == $routeParams.id) {
-                $scope.item = data.items[i];
-                setCookie("html_player", 1, 365);
-                break;
+ 
+    $scope.item = [];
+    $scope.url = 'templates/detailedView.json';
+    $scope.getIframeSrc = function (videoURL) {
+        return 'videoURL';
+    };
+
+    $scope.fetchContent = function () {
+        $http.get($scope.url, { cache: true }).success(function (data) {
+            for (var i = 0; i < data.items.length; i++) {
+                if (data.items[i].detailedLinkID == $routeParams.id) {
+                    $scope.item = data.items[i];
+                    // setCookie("html_player", 1, 365);
+                    break;
+                }
             }
-        }
-    }).error(function (data) {
-        $window.location.href = '#/Error';
-    });
+        }).error(function (data) {
+            $window.location.href = '#/Error';
+        });
+
+
+    }
+    $scope.fetchContent();
+
     $http.get("templates/homepage.json", { cache: true }).success(function (data) {
         $scope.list = data.items;
     }).error(function (data) {
@@ -57,3 +71,12 @@ avenue.filter("nl2br", ['$sce', function ($sce) {
         return $sce.trustAsHtml(text);
     };
 }]);
+
+
+avenue.filter('youtubeEmbedUrl', function ($sce) {
+    return function (videoURL) {
+        return $sce.trustAsResourceUrl(videoURL);
+    };
+});
+
+ })(window.angular);
